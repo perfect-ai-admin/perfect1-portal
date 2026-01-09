104fb3fa9a7a45e14e9cf18c2a4b44f7bb7baf85
יוסי לוי <yosi5919@gmail.com>
2026-01-09 14:48:37 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -197,7 +197,7 @@ export default function About() {
       </div>
 
       {/* Values */}
-      <section className="py-20 bg-white">
+      <section className="py-16 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
