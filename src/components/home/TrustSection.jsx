f039b555f10fdc88ea8b6cb3865575b325e0fe6c
יוסי לוי <yosi5919@gmail.com>
2026-01-10 08:35:07 +0000

    File changes

--- src/components/home/TrustSection.jsx
+++ src/components/home/TrustSection.jsx
@@ -32,7 +32,7 @@ export default function TrustSection() {
         </motion.div>
 
         {/* Trust Cards Grid */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {trustPoints.map((item, index) => {
             const Icon = item.icon;
             return (
